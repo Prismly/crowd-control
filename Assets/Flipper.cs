ece653f36e02dd250a01a0388ae2f76c467c1c28
Isaac Kellogg <keldtdev@gmail.com>
2023-01-30 21:26:15 -0500

    Added pinball-style flippers
    There's a left and a right variant for these: Flipper_L and Flipper_R.
    
    Test scene: "Interactive Elements Test 3"

---
+++ Assets/Flipper.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour
+{
+    private HingeJoint hinge;
+    public bool OnLeft = true;
+    public string AxisName = "Horizontal";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hinge = gameObject.GetComponent<HingeJoint>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        JointSpring hingeSpring = hinge.spring;
+        JointLimits hingeLimits = hinge.limits;
+
+        bool SwingBat = (Mathf.RoundToInt(Input.GetAxisRaw("Horizontal")) < 0);
+        if(!OnLeft)
+        {
+            SwingBat = (Mathf.RoundToInt(Input.GetAxisRaw("Horizontal")) > 0);
+        }
+
+        if(SwingBat)
+        {
+            hingeSpring.targetPosition = hingeLimits.max;
+        }
+        else
+        {
+            hingeSpring.targetPosition = hingeLimits.min;
+        }
+        Debug.Log(hingeSpring.targetPosition);
+        hinge.spring = hingeSpring;
+    }
+}
